807aff2ec7fad1d03e0cfafde11bc0ccbc8b366e
Umesh Unnikrishnan <umeshanfloyd@gmail.com>
2021-12-20 17:00:43 -0300

    added controller, router and config. for now keeping env file inside the source

---
+++ controllers/activityController.js
@@ -0,0 +1,1 @@
+const Activity = require("../models/activityModel");
--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4000;
+
+//Enabling json accpetion using middleware
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//Accpeting cors
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+//using middleware to accept cors
+app.use(cors(corsOptions));
+
+// //Initiating routing
+// const fnaRouter = require("./routes/fnTrackerRoutes");
+
+// //using middleware for all requests receving starts with subscribers
+// app.use("/fna", fnaRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
